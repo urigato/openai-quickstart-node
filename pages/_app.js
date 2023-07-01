ad47ac7ae7c55cd8d8498633ec43d1309872fcbc
gychoi <gychoi@gatolab.com>
2023-07-02 01:21:22 +0900

    구글 어시스턴스추가

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,21 @@
 // 1. import `NextUIProvider` component
+import Script from 'next/script';
 import { NextUIProvider } from "@nextui-org/react";
 
 function MyApp({ Component, pageProps }) {
   return (
     // 2. Use at the root of your app
     <NextUIProvider>
+        <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-X114QSSVG5"></Script>
+        <Script id="google-analytics" strategy="afterInteractive">
+            {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-X114QSSVG5');
+            `}
+        </Script>
       <Component {...pageProps} />
     </NextUIProvider>
   );
